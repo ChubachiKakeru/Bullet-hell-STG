196a40b72908a7e372d3434c9856c709a1024113
kiyoto <sdt2495gp0001@edu.sca.ac.jp>
2025-11-25 18:16:23 +0900

    プレイヤーがなぜか弾を２発撃つようになってしまった

--- ひな形/project/Source/PlayScene.cpp
+++ ひな形/project/Source/PlayScene.cpp
@@ -26,48 +26,16 @@ void PlayScene::Update()
 		SceneManager::ChangeScene("TITLE");
 	}
 
-	// プレイヤー更新
-	Player* player = FindGameObject<Player>();
-	if (player != nullptr) {
-		player->Update();
-	}
-
-	// ボス更新
-	Boss1* boss = FindGameObject<Boss1>();
-	if (boss != nullptr) {
-		boss->Update();
-	}
-
-	// 弾の判定処理
-	CheckCollisions();
+	
 }
 
 void PlayScene::CheckCollisions() {
-    Player* player = FindGameObject<Player>();
-    Boss1* boss = FindGameObject<Boss1>();
-
-    if (player == nullptr || !player->IsActive()) return;
-
-    // プレイヤーの弾 vs ボス
-    if (boss != nullptr && boss->IsActive()) {
-        // 全ての弾をチェック（仮想コード）
-       /*  for (Bullet* bullet : playerBullets) {
-             if (bullet->IsActive()) {
-                 bool hit = CollisionManager::CheckCollision(
-                     bullet->GetX(), bullet->GetY(), 
-                     bullet->GetRectW(), bullet->GetRectH(), bullet->GetRadius(),
-                     boss->GetX(), boss->GetY(), 
-                     boss->GetRectW(), boss->GetRectH(), boss->GetRadius()
-                 );
-                 
-                 if (hit) {
-                     boss->TakeDamage(10);
-                     bullet->Deactivate();
-                 }
-             }
-         }*/
-    }
+   
 }
+
+//void PlayScene::RemoveDeadBullets(std::vector<Bullet*>& bullets)
+//{
+//}
 
 void PlayScene::Draw()
 {
--- ひな形/project/Source/PlayScene.h
+++ ひな形/project/Source/PlayScene.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "../Library/SceneBase.h"
+#include "Player.h"
+#include "Boss1.h"
+#include "Bullet.h"
+#include <vector>
+
 
 /// <summary>
 /// ゲームプレイのシーンを制御する
@@ -11,5 +16,16 @@ public:
 	~PlayScene();
 	void Update() override;
 	void Draw() override;
-	void CheckCollisions();
+
+private:
+    Player player;
+    Boss1 boss;
+    std::vector<Bullet> playerBullets;
+    std::vector<Bullet> bossBullets;
+
+    // 当たり判定用の関数
+    void CheckCollisions();
+    void RemoveDeadBullets(std::vector<Bullet*>& bullets);  // 死んだ弾削除用
+
+
 };
